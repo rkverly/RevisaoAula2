2b81691204c028c8f50de2b032533a6caf17612c
ninguem <ninguem>
2023-06-06 14:19:19 -0300

    Implementando a leitura de dois números

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
-we
+# include <stdio.h>
+
+int main(){
+    float num1, num2;
+    float soma, subtracao, multiplicacao, divisao;
+
+    printf("Digite o primeiro número: ");
+    scanf("%f", &num1);
+
+    printf("\nDigite o segundo número: ");
+    scanf("%f", &num2);
+
+
+
+    return 0;
+
+}
+
